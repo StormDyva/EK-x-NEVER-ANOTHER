796901ae219e7c50621c7d61e41a1dde0eb2f6c2
Loui <Loui.fatum@gmail.com>
2026-05-20 16:31:38 +0200

    Fonts

---
+++ app/src/main/java/com/example/neveranother/ui/theme/Font.kt
@@ -0,0 +1,26 @@
+package com.example.neveranother.ui.theme
+
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.neveranother.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_Regular, FontWeight.Normal),
+    Font(R.font.inter_Medium, FontWeight.Medium),
+    Font(R.font.inter_SemiBold, FontWeight.SemiBold),
+    Font(R.font.inter_Bold, FontWeight.Bold)
+)
+
+val InterDisplay = FontFamily(
+    Font(R.font.interDisplay_Regular, FontWeight.Normal),
+    Font(R.font.interDisplay_Bold, FontWeight.Bold)
+)
+val Nohemi = FontFamily(
+    Font(R.font.nohemi_Regular, FontWeight.Normal),
+    Font(R.font.nohemi_Medium, FontWeight.Medium),
+    Font(R.font.nohemi_SemiBold, FontWeight.SemiBold),
+    Font(R.font.nohemi_Bold, FontWeight.Bold)
+)
+
