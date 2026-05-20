cd68c3f4f68c2c27ad25986b3c38961819d4c6ca
frederikjohansen <frederik.egede.johansen@gmail.com>
2026-05-20 13:20:32 +0200

    First class?

---
+++ app/src/main/java/com/example/neveranother/model/Bh.kt
@@ -0,0 +1,10 @@
+package com.example.neveranother.model
+
+abstract class Bh (
+    val upperCircumference: Float,
+    val lowerCircumference: Float,
+    val breastSpan: Float,
+    val breastHeight: Float
+){
+    abstract val color: String
+}
