571be77e7bd3ece963dbc2dd7af1c4c13aaf47c4
StormDyva <storm.dyva@gmail.com>
2026-05-21 23:04:48 +0200

    DesignAndProduct done, samt rettelser fikset BackBtn

--- app/src/main/java/com/example/neveranother/MainActivity.kt
+++ app/src/main/java/com/example/neveranother/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : ComponentActivity() {
 
             ) {
                 Column {
-                    NavHost(navController = navController, startDestination = "Home") {
+                    NavHost(navController = navController, startDestination = "BookFitting-screen") {
 
                         composable("Basket-screen") {
                             Basket(navController, vm)
