2d98d8d3f60fce0c6cf0d5ee4e098ff723d9e275
Loui <Loui.fatum@gmail.com>
2026-05-26 13:06:55 +0200

    video til info

---
+++ app/src/main/java/com/example/neveranother/screen/InfoVideoNR3.kt
@@ -0,0 +1,46 @@
+package com.example.neveranother.screen
+
+import android.widget.VideoView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+import com.example.neveranother.viewModel.NAViewmodel
+import com.example.neveranother.R
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
+import com.example.neveranother.components.BackBTN
+
+@Composable
+fun InfoVideoNR3(
+    navController: NavHostController,
+    vm: NAViewmodel
+) {
+    val context = LocalContext.current
+
+    val videoUri = "android.resource://${context.packageName}/${R.raw.measure_video_3}".toUri()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                VideoView(context).apply {
+                    setVideoURI(videoUri)
+                    start()
+                }
+            }
+        )
+    }
+    BackBTN(
+        { navController.popBackStack() },
+        color = Color.White
+    )
+}
